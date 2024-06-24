94c15233a53ad2eb9a9e4fd3893a7ab44a151be3
DemenNgoding <92081780+DemenNgoding@users.noreply.github.com>
2024-06-24 23:10:11 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,20 @@ public class Main {
         try {
             Searching search = new Searching();
 
+            // hal pertama yang harus diapstikan adalah koneksi database yang terletak pada file Searching.java pastikan username password dan database yang dimasukan adalah keterangan database yang sesuai dengan yang terinstall pada komputer anda
+            
+            // disini kita menggunakan database postgre sql
+
+            // jika ingin running code nya maka compile file terlebih dahulu dengan command
+            // javac -cp " didalam tanda kutip ini diisi path file postgresql-42.7.3.jar yang berada di folder lib" KoneksiDB.java BTree.java Searching.java Main.java
+
+            // lalu jika ingin run code nya maka basukan command
+            //java -cp " didalam tanda kutip ini diisi path file postgresql-42.7.3.jar yang berada di folder lib" Main.java
+
+            
+            System.out.println("Data yang berada pada database disini adalah sebanyak 64037 silahkan memilih id stasiun mana ayng ingin dicari antara 1 - 64037");
+            System.out.println("===================");
+
             Scanner scanner = new Scanner(System.in);
             System.out.print("Input ID = ");
             int id = scanner.nextInt();
